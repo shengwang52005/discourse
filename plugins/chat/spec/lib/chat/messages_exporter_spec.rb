dc2cef09fda53c785787e098f7111858f28c8d5d
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-08-17 16:50:45 +0400

    Use contexts

--- plugins/chat/spec/lib/chat/messages_exporter_spec.rb
+++ plugins/chat/spec/lib/chat/messages_exporter_spec.rb
@@ -1,46 +1,56 @@
 # frozen_string_literal: true
 
 describe Chat::MessagesExporter do
-  fab!(:public_channel) { Fabricate(:chat_channel) }
-  fab!(:public_channel_message_1) { Fabricate(:chat_message, chat_channel: public_channel) }
-  fab!(:public_channel_message_2) { Fabricate(:chat_message, chat_channel: public_channel) }
-  # this message is deleted in the before block:
-  fab!(:deleted_message) { Fabricate(:chat_message, chat_channel: public_channel) }
+  context "with a standard set up" do
+    fab!(:public_channel) { Fabricate(:chat_channel) }
+    fab!(:public_channel_message_1) { Fabricate(:chat_message, chat_channel: public_channel) }
+    fab!(:public_channel_message_2) { Fabricate(:chat_message, chat_channel: public_channel) }
+    # this message is deleted in the before block:
+    fab!(:deleted_message) { Fabricate(:chat_message, chat_channel: public_channel) }
 
-  fab!(:private_channel) { Fabricate(:private_category_channel, group: Fabricate(:group)) }
-  fab!(:private_channel_message_1) { Fabricate(:chat_message, chat_channel: private_channel) }
-  fab!(:private_channel_message_2) { Fabricate(:chat_message, chat_channel: private_channel) }
+    fab!(:private_channel) { Fabricate(:private_category_channel, group: Fabricate(:group)) }
+    fab!(:private_channel_message_1) { Fabricate(:chat_message, chat_channel: private_channel) }
+    fab!(:private_channel_message_2) { Fabricate(:chat_message, chat_channel: private_channel) }
 
-  fab!(:user_1) { Fabricate(:user) }
-  fab!(:user_2) { Fabricate(:user) }
-  fab!(:dm_channel) { Fabricate(:direct_message_channel, users: [user_1, user_2]) }
-  fab!(:direct_message_1) { Fabricate(:chat_message, chat_channel: private_channel, user: user_1) }
-  fab!(:direct_message_2) { Fabricate(:chat_message, chat_channel: private_channel, user: user_2) }
+    fab!(:user_1) { Fabricate(:user) }
+    fab!(:user_2) { Fabricate(:user) }
+    fab!(:dm_channel) { Fabricate(:direct_message_channel, users: [user_1, user_2]) }
+    fab!(:direct_message_1) do
+      Fabricate(:chat_message, chat_channel: private_channel, user: user_1)
+    end
+    fab!(:direct_message_2) do
+      Fabricate(:chat_message, chat_channel: private_channel, user: user_2)
+    end
 
-  before { deleted_message.trash! }
+    before { deleted_message.trash! }
 
-  it "exports messages" do
-    exporter = Class.new.extend(Chat::MessagesExporter)
+    it "exports messages" do
+      exporter = Class.new.extend(Chat::MessagesExporter)
 
-    result = []
-    exporter.chat_message_export { |data_row| result << data_row }
+      result = []
+      exporter.chat_message_export { |data_row| result << data_row }
 
-    expect(result.length).to be(7)
-    assert_exported_message(result[0], public_channel_message_1)
-    assert_exported_message(result[1], public_channel_message_2)
-    assert_exported_message(result[2], deleted_message)
-    assert_exported_message(result[3], private_channel_message_1)
-    assert_exported_message(result[4], private_channel_message_2)
-    assert_exported_message(result[5], direct_message_1)
-    assert_exported_message(result[6], direct_message_2)
+      expect(result.length).to be(7)
+      assert_exported_message(result[0], public_channel_message_1)
+      assert_exported_message(result[1], public_channel_message_2)
+      assert_exported_message(result[2], deleted_message)
+      assert_exported_message(result[3], private_channel_message_1)
+      assert_exported_message(result[4], private_channel_message_2)
+      assert_exported_message(result[5], direct_message_1)
+      assert_exported_message(result[6], direct_message_2)
+    end
   end
 
-  it "exports messages from deleted channels" do
-    # not implemented
+  context "with messages in deleted channels" do
+    it "exports such messages" do
+      # not implemented
+    end
   end
 
-  it "export messages of deleted users" do
-    # not implemented
+  context "with messages of deleted users" do
+    it "exports such messages" do
+      # not implemented
+    end
   end
 
   def assert_exported_message(data_row, message)
