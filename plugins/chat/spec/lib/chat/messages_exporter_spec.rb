7da19e725fd0000d0f58f0258a2b77f09a81b1db
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-08-17 23:25:49 +0400

    Use unscoped in assertions too

--- plugins/chat/spec/lib/chat/messages_exporter_spec.rb
+++ plugins/chat/spec/lib/chat/messages_exporter_spec.rb
@@ -57,18 +57,20 @@ describe Chat::MessagesExporter do
   end
 
   def assert_exported_message(data_row, message)
-    expect(data_row[0]).to eq(message.id)
-    expect(data_row[1]).to eq(message.chat_channel_id)
-    #expect(data_row[2]).to eq(message.chat_channel.name)
-    expect(data_row[3]).to eq(message.user.id)
-    expect(data_row[4]).to eq(message.user.username)
-    expect(data_row[5]).to eq(message.message)
-    expect(data_row[6]).to eq(message.cooked)
-    expect(data_row[7]).to eq_time(message.created_at)
-    expect(data_row[8]).to eq_time(message.updated_at)
-    expect(data_row[9]).to eq_time(message.deleted_at)
-    expect(data_row[10]).to eq(message.in_reply_to_id)
-    expect(data_row[11]).to eq(message.last_editor.id)
-    expect(data_row[12]).to eq(message.last_editor.username)
+    Chat::Channel.unscoped do
+      expect(data_row[0]).to eq(message.id)
+      expect(data_row[1]).to eq(message.chat_channel.id)
+      expect(data_row[2]).to eq(message.chat_channel.name)
+      expect(data_row[3]).to eq(message.user.id)
+      expect(data_row[4]).to eq(message.user.username)
+      expect(data_row[5]).to eq(message.message)
+      expect(data_row[6]).to eq(message.cooked)
+      expect(data_row[7]).to eq_time(message.created_at)
+      expect(data_row[8]).to eq_time(message.updated_at)
+      expect(data_row[9]).to eq_time(message.deleted_at)
+      expect(data_row[10]).to eq(message.in_reply_to_id)
+      expect(data_row[11]).to eq(message.last_editor.id)
+      expect(data_row[12]).to eq(message.last_editor.username)
+    end
   end
 end
