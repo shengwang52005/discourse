a05776d3fc02fc6f88be2c00f2119ff93b8b5e52
Mark VanLandingham <markvanlan@gmail.com>
2023-08-24 14:39:48 -0500

    DEV: Use Notice API for mention warnings (tests not fixed)

--- plugins/chat/assets/javascripts/discourse/services/chat-pane-base-subscriptions-manager.js
+++ plugins/chat/assets/javascripts/discourse/services/chat-pane-base-subscriptions-manager.js
@@ -1,6 +1,5 @@
 import Service, { inject as service } from "@ember/service";
 import ChatMessage from "discourse/plugins/chat/discourse/models/chat-message";
-import ChatMessageMentionWarning from "discourse/plugins/chat/discourse/models/chat-message-mention-warning";
 import { cloneJSON } from "discourse-common/lib/object";
 import { bind } from "discourse-common/utils/decorators";
 
@@ -101,9 +100,6 @@ export default class ChatPaneBaseSubscriptionsManager extends Service {
       case "restore":
         this.handleRestoreMessage(busData);
         break;
-      case "mention_warning":
-        this.handleMentionWarning(busData);
-        break;
       case "self_flagged":
         this.handleSelfFlaggedMessage(busData);
         break;
@@ -194,13 +190,6 @@ export default class ChatPaneBaseSubscriptionsManager extends Service {
       const newMessage = ChatMessage.create(this.model, data.chat_message);
       newMessage.manager = this.messagesManager;
       this.messagesManager.addMessages([newMessage]);
-    }
-  }
-
-  handleMentionWarning(data) {
-    const message = this.messagesManager.findMessage(data.chat_message_id);
-    if (message) {
-      message.mentionWarning = ChatMessageMentionWarning.create(message, data);
     }
   }
 
