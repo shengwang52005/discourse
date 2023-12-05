be2fcbed8783cb133c2faea55f58aed81f7a51b5
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-12-05 19:41:12 +0400

    MIGRATION: add new columns and migrate data

--- db/migrate/20231205151833_add_type_and_target_id_to_chat_mentions.rb
+++ db/migrate/20231205151833_add_type_and_target_id_to_chat_mentions.rb
@@ -2,11 +2,16 @@
 
 class AddTypeAndTargetIdToChatMentions < ActiveRecord::Migration[7.0]
   def change
-    # add the type column
-    # add the target id column
+    add_column :chat_mentions, :type, :integer, null: true
+    add_column :chat_mentions, :target_id, :integer, null: true
+
+    DB.exec <<~SQL
+      UPDATE chat_mentions
+      SET type = 1, target_id = user_id;
+    SQL
+
     # fixme andrei add indexes
     # fixme andrei take care of old indexes
-    # migrate data
     # fixme andrei make type column not nullable
   end
 end
