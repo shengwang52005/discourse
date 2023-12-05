ad2ac4e1d8a515a122cf29fd13bf68992801f2f3
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-12-05 19:32:05 +0400

    Draft a migration

---
+++ db/migrate/20231205151833_add_type_and_target_id_to_chat_mentions.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class AddTypeAndTargetIdToChatMentions < ActiveRecord::Migration[7.0]
+  def change
+    # add the type column
+    # add the target id column
+    # fixme andrei add indexes
+    # fixme andrei take care of old indexes
+    # migrate data
+    # fixme andrei make type column not nullable
+  end
+end
