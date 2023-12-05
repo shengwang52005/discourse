89496743ed5232329bdf105d0fa7b7318a36bb58
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-12-05 20:03:03 +0400

    MIGRATION: make migration reversible

--- db/migrate/20231205151833_add_type_and_target_id_to_chat_mentions.rb
+++ db/migrate/20231205151833_add_type_and_target_id_to_chat_mentions.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AddTypeAndTargetIdToChatMentions < ActiveRecord::Migration[7.0]
-  def change
+  def up
     add_column :chat_mentions, :type, :integer, null: true
     add_column :chat_mentions, :target_id, :integer, null: true
 
@@ -13,5 +13,10 @@ class AddTypeAndTargetIdToChatMentions < ActiveRecord::Migration[7.0]
     # fixme andrei add indexes
     # fixme andrei take care of old indexes
     # fixme andrei make type column not nullable
+  end
+
+  def down
+    remove_column :chat_mentions, :type
+    remove_column :chat_mentions, :target_id
   end
 end
