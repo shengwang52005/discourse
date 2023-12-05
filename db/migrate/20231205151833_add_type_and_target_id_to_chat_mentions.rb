a2f2594a7517c6090922bed88e59f286c9c9b08b
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-12-05 23:00:37 +0400

    MIGRATION: correct migration to support inheritence

--- db/migrate/20231205151833_add_type_and_target_id_to_chat_mentions.rb
+++ db/migrate/20231205151833_add_type_and_target_id_to_chat_mentions.rb
@@ -3,17 +3,17 @@
 class AddTypeAndTargetIdToChatMentions < ActiveRecord::Migration[7.0]
   def up
     change_column :chat_mentions, :user_id, :integer, null: true
-    add_column :chat_mentions, :type, :integer, null: true
+    add_column :chat_mentions, :type, :string, null: true
     add_column :chat_mentions, :target_id, :integer, null: true
 
     DB.exec <<~SQL
       UPDATE chat_mentions
-      SET type = 1, target_id = user_id;
+      SET type = 'UserMention', target_id = user_id;
     SQL
 
     # fixme andrei add indexes
     # fixme andrei take care of old indexes
-    # fixme andrei make type column not nullable
+    # fixme andrei make the type column not nullable
   end
 
   def down
