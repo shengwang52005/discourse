ce0c225dd3553103e02e707cb8712ca373c40ccd
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-02-15 20:41:34 +0400

    Do not check Notifier's returned value in more tests

--- plugins/chat/spec/jobs/regular/chat/process_message_spec.rb
+++ plugins/chat/spec/jobs/regular/chat/process_message_spec.rb
@@ -65,13 +65,13 @@ describe Jobs::Chat::ProcessMessage do
       end
 
       shared_examples "channel-wide mentions" do
-        it "returns an empty list when the message doesn't include a channel mention" do
+        it "doesn't create notifications when the message doesn't include a channel mention" do
           msg = build_cooked_msg(mention.gsub("@", ""), user_1)
           Fabricate(mention_type, chat_message: msg)
 
-          to_notify = Chat::Notifier.new(msg, msg.created_at).notify_new
+          Chat::Notifier.new(msg, msg.created_at).notify_new
 
-          expect(to_notify[list_key]).to be_empty
+          expect(Notification.count).to be(0)
         end
 
         it "will never include someone who is not accepting channel-wide notifications" do
